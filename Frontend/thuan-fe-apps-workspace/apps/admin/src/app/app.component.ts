e9b86d16e295a51f03e09b54c05276997d64846d
Duong Minh Thuan <duongminhthuan1478@gmail.com>
2022-01-25 15:55:32 +0700

    Fronend: Using init create work-space with 2 applications E-Shop | Admin

---
+++ Frontend/thuan-fe-apps-workspace/apps/admin/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'thuan-fe-apps-workspace-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'admin';
+}
