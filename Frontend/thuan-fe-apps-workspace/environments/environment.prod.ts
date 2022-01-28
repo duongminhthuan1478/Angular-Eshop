42941016456202a9f06ebdb6a91d34ae33d322b3
Duong Minh Thuan <duongminhthuan1478@gmail.com>
2022-01-28 16:31:47 +0700

    Fronend: Remove env files  in Admin application, using env at workspace level instead

---
+++ Frontend/thuan-fe-apps-workspace/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+  export const environment = {
+    production: true,
+    API_URL: 'http://localhost:3000/api/v1/'
+  };
+  
--- Frontend/thuan-fe-apps-workspace/libs/products/src/lib/services/categories.service.ts
+++ Frontend/thuan-fe-apps-workspace/libs/products/src/lib/services/categories.service.ts
@@ -2,31 +2,33 @@ import { Category } from './../models/Category';
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http'
 import { Observable } from 'rxjs';
+import { environment } from '@thuan-env/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesService {
+  private API_CATEGORIES = environment.API_URL + 'categories';
 
   constructor(private http: HttpClient) { }
 
   getCategories(): Observable<any> {
-    return this.http.get<any>('http://localhost:3000/api/v1/categories/');
+    return this.http.get<any>(this.API_CATEGORIES);
   }
 
   getCategoryById(catId: string): Observable<any> {
-    return this.http.get<any>(`http://localhost:3000/api/v1/categories/getCategoryById/${catId}`);
+    return this.http.get<any>(`${this.API_CATEGORIES}/getCategoryById/${catId}`);
   }
 
   createCategory(category : Category): Observable<any> {
-    return this.http.post('http://localhost:3000/api/v1/categories/create', category);
+    return this.http.post(`${this.API_CATEGORIES}/create`, category);
   }
 
   updateCategory(category : Category): Observable<any> {
-    return this.http.put(`http://localhost:3000/api/v1/categories/update/${category.id}`, category);
+    return this.http.put(`${this.API_CATEGORIES}/update/${category.id}`, category);
   }
 
   deleteCategory(categoryId: string): Observable<any> {
-    return this.http.delete(`http://localhost:3000/api/v1/categories/delete/${categoryId}`);
+    return this.http.delete(`${this.API_CATEGORIES}/delete/${categoryId}`);
   }
 }
